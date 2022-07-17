8e3d25fe839d9690d78d8aeba707a0b659975158
jwelyl <cork2586@naver.com>
2022-07-18 00:01:34 +0900

    220718 [ADD]

--- java_theory/deepCopy.cpp
+++ java_theory/deepCopy.cpp
@@ -14,13 +14,15 @@ int main(void) {
     for(int i = 0; i < 10; i++)
         v1.push_back(i);
 
-    v2 = v1;
+    v2 = v1;    //  v2에 v1 복사(deep copy)
+    cout << "v2에 v1 복사 후 v1, v2 출력\n";
     printVector(v1);
     printVector(v2);
 
-    for(int i = 0; i < 10; i += 2)
+    for(int i = 0; i < 10; i += 2)  //  v2 수정
         v2[i] *= 2;
 
+    cout << "v2 수정 후 v1, v2 출력\n";
     printVector(v1);
     printVector(v2);
 
